aa2cda8c737ada27fe3670ff8d517dd4368747c0
dangerous <gm.davi.gm@live.com>
2018-08-15 20:47:46 -0300

    Ajustes no math.h

--- Trabalho 3/math.h
+++ Trabalho 3/math.h
@@ -5,9 +5,9 @@
 #ifndef LABORATORIO_MATH_H
 #define LABORATORIO_MATH_H
 
-#endif //LABORATORIO_MATH_H
-
 long double sum(long double a, long double b);
 long double subtract(long double a, long double b);
 long double times(long double a, long double b);
 long double divide(long double a, long double b);
+
+#endif //LABORATORIO_MATH_H
